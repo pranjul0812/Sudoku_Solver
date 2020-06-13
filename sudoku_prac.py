a7ffd803009195c7bb359607b40ddadf35007f9c
Pranjul Mishra <MTK06013@apj.mediatek.inc>
2020-06-14 04:12:12 +0530

    updated solution

--- sudoku_prac.py
+++ sudoku_prac.py
@@ -152,7 +152,69 @@ def checkOtherValues(sudo_sample2, i, j, k, l):
 	return sudo_sample2, found
 
 
-def solve_game(sudo_sample1):
+def checkBlock(lst, i, j):
+	expected_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+	zero_positions = []
+	change = False
+	for k in range(3):
+		for l in range(3):
+			if lst[i][j][k][l] != 0:
+				expected_list.remove(lst[i][j][k][l])
+			else:
+				zero_positions.append([k, l])
+	# print(expected_list)
+	# print(zero_positions)
+	for item in expected_list:
+		possible_positions = []
+		for position in zero_positions:
+			if checkPossibility(lst, i, j, position, item):
+				possible_positions.append(position)
+			if len(possible_positions) > 1:
+				break
+		# print(possible_positions)
+		if len(possible_positions) == 1:
+			change = True
+			lst[i][j][possible_positions[0][0]][possible_positions[0][1]] = item
+			zero_positions.remove(possible_positions[0])
+
+	return lst, change
+
+
+def checkPossibility(lst, i, j, position, item):
+	k = position[0]
+	l = position[1]
+	# check in vertical values:
+	for o_row in range(3):
+		for i_row in range(3):
+			# print(lst[o_row][j][i_row][l], end=" ")
+			if lst[o_row][j][i_row][l] == item:
+				return False
+	# print()
+	# check in horizontal values:
+	for o_col in range(3):
+		for i_col in range(3):
+			# print(lst[i][o_col][k][i_col], end=" ")
+			if lst[i][o_col][k][i_col] == item:
+				return False
+
+	return True
+
+
+def solve_game_level2(sudo_sample):
+	# go in each block and check if we can fill any values
+	fill = False
+	for i in range(3):
+		for j in range(3):
+			sudo_sample, change = checkBlock(sudo_sample, i, j)
+			time.sleep(3)
+			if change:
+				fill = True
+				# os.system('cls')
+				# display_sudo(sudo_sample)
+	return sudo_sample, fill
+
+
+def solve_game_level1(sudo_sample1):
 	result = sudo_sample1
 	found = False
 	fill = False
@@ -165,8 +227,8 @@ def solve_game(sudo_sample1):
 						result, found = checkOtherValues(sudo_sample1, i, j, k, l)
 					if found:
 						fill = True
-						os.system('cls')
-						display_sudo(result)
+						# os.system('cls')
+						# display_sudo(result)
 
 	return result, fill
 
@@ -186,11 +248,22 @@ def start_game():
 	print("Let's start solving the sudoku")
 	check = True
 	while check:
-		sudo_sample, fill = solve_game(sudo_sample)
+		sudo_sample, fill = solve_game_level1(sudo_sample)
 		if not fill:
 			check = False
 		else:
 			check = zeroAvailable(sudo_sample)
+	# os.system('cls')
+	# print("Your Level1 Solved sudoku is coming in few seconds.. ")
+	# time.sleep(.5)
+	# display_sudo(sudo_sample)
+	# time.sleep(1)
+	while not check:
+		sudo_sample, fill = solve_game_level2(sudo_sample)
+		if not fill:
+			check = True
+		else:
+			check = not zeroAvailable(sudo_sample)
 	time.sleep(1)
 	os.system('cls')
 	print("Your Final Solved sudoku is coming in few seconds.. ")
